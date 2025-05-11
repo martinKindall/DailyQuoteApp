149143ab699fb186cec56d982dd5abef86bd4193
Martin Cornejo <mart256@gmail.com>
2025-05-12 00:51:02 +0200

    Added resources for the app

--- lib/quotes_app-stack.ts
+++ lib/quotes_app-stack.ts
@@ -1,16 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
 
 export class QuotesAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const storeBucket = new s3.Bucket(this, "Morsa_Programando_AppQuotes");
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'QuotesAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const lambdaLogic = new lambda.Function(this, "AppQuotesLambda", {
+      runtime: lambda.Runtime.JAVA_21,
+      handler: "com.codigomorsa.app_quotes.App::handleRequest",
+      code: lambda.Code.fromAsset("./app/build/libs/apps..."),   // TODO
+      memorySize: 256,
+      timeout: cdk.Duration.seconds(30)
+    });
+
+    storeBucket.grantReadWrite(lambdaLogic);
   }
 }
